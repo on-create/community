b81106ad23dd4622c83cf17668575f3b1ed2e133
on-create <zhong.ming.shan@outlook.com>
2020-11-14 20:19:17 +0800

    ä¿完善问题列表展示 add 2020-11-14 20:19

--- src/main/java/com/example/community/controller/IndexController.java
+++ src/main/java/com/example/community/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.example.community.controller;
 
+import com.example.community.dto.PaginationDTO;
 import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -22,7 +24,10 @@ public class IndexController {
     private QuestionService questionService;
 
     @GetMapping("/")
-    public String index(HttpServletRequest request, Model model) {
+    public String index(HttpServletRequest request,
+                        Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size) {
         //获取Cookie数组
         Cookie[] cookies = request.getCookies();
 
@@ -44,8 +49,8 @@ public class IndexController {
         }
 
         //获取问题集合
-        List<QuestionDTO> questions = questionService.list();
-        model.addAttribute("questions", questions);
+        PaginationDTO pagination = questionService.list(page, size);
+        model.addAttribute("pagination", pagination);
 
         return "index";
     }
--- src/main/java/com/example/community/service/QuestionService.java
+++ src/main/java/com/example/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package com.example.community.service;
 
+import com.example.community.dto.PaginationDTO;
 import com.example.community.dto.QuestionDTO;
 import com.example.community.mapper.QuestionMapper;
 import com.example.community.mapper.UserMapper;
@@ -21,21 +22,49 @@ public class QuestionService {
     @Autowired(required = false)
     private QuestionMapper questionMapper;
 
-    public List<QuestionDTO> list() {
-        //查找question对象
-        List<Question> questions = questionMapper.list();
-        List<QuestionDTO> questionDTOS = new ArrayList<QuestionDTO>();
+    public PaginationDTO list(Integer page, Integer size) {
+        //获取数据库中question数据的总条数
+        Integer totalCount =questionMapper.count();
+        //总页数
+        Integer totalPage;
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+        //限制page
+        if (page < 1) {
+            page = 1;
+        } else if (page > totalPage) {
+            page = totalPage;
+        }
+        //数据库查找索引，page=1时从第一条开始
+        Integer offset = size * (page - 1);
+
+        //从数据库中查找部分question集合数据
+        List<Question> questions = questionMapper.list(offset, size);
+        //创建questionDTO集合对象
+        List<QuestionDTO> questionDTOS = new ArrayList<>();
+        //创建分页部分question对象
+        PaginationDTO paginationDTO = new PaginationDTO();
 
         //遍历questions
         for (Question question : questions) {
+            //通过问题提交用户id查找用户
             User user = userMapper.findById(question.getCreator());
+            //创建questionDTO对象
             QuestionDTO questionDTO = new QuestionDTO();
             //将question转换为questionDTO
             BeanUtils.copyProperties(question, questionDTO);
+            //给questionDTO中的user属性赋值
             questionDTO.setUser(user);
             //将questionDTO添加进questionDTOS中
             questionDTOS.add(questionDTO);
         }
-        return questionDTOS;
+
+        //将questionDTO添加进paginationDTO中
+        paginationDTO.setQuestions(questionDTOS);
+        paginationDTO.setPagination(totalPage, page, size);
+        return paginationDTO;
     }
 }
